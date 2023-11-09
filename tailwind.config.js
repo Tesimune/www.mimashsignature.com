2abe22d57eed3c4ffbc6da46a882daaa527fb5bc
Tesimune <tesimune@gmail.com>
2023-11-09 05:04:23 +0100

    React-icon and daisyui added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,27 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
+const colors = require("tailwindcss/colors");
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.jsx',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.jsx",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
             },
+        },
+        colors: {
+            ...colors,
+            gold: colors.amber["600"],
         },
     },
 
-    plugins: [forms],
+    plugins: [forms, require("daisyui")],
 };
