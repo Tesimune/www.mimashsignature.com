9e3bdb86a134cacb545498fd3c2f72efe89d4303
Tesimune <tesimune@gmail.com>
2023-12-23 11:49:36 +0100

    Build updated

--- public/build/assets/Index-46557e84.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./app-08956282.js";import{A as r}from"./AuthenticatedLayout-183893a7.js";import"./index.esm-a3e50ae0.js";import"./transition-28bfd579.js";import"./iconBase-c6179dd5.js";import"./index.esm-404e3d85.js";function s(){return t.jsx(r,{children:"Index"})}export{s as default};
--- public/build/assets/Show-a768b04e.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./app-08956282.js";import{A as r}from"./AuthenticatedLayout-183893a7.js";import"./index.esm-a3e50ae0.js";import"./transition-28bfd579.js";import"./iconBase-c6179dd5.js";import"./index.esm-404e3d85.js";function a({orders:o}){return console.log(o),t.jsx(r,{})}export{a as default};
