855bb1638a6b16780b3b59ea1f0190fd5578128e
Tesimune <tesimune@gmail.com>
2024-01-11 07:15:49 +0100

    Updated products and category routes

--- routes/web/products.php
+++ routes/web/products.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\ProductCategoryController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -14,4 +15,10 @@ Route::middleware('auth')->prefix("/mystore/{store:username}/")->name("product."
     Route::get('/{product:id}', [ProductsController::class, 'edit'])->name('edit');
     Route::put('/update/{product}', [ProductsController::class, 'update'])->name('update');
     Route::delete('/delete/{product}', [ProductsController::class, 'destroy'])->name('destroy');
+});
+
+Route::middleware('auth')->prefix("/mystore/{store:username}/category/")->name("category.")->group(function () {
+    Route::post('/store', [ProductCategoryController::class, 'store'])->name('store');
+    Route::put('/update/{category}', [ProductCategoryController::class, 'update'])->name('update');
+    Route::delete('/delete/{category}', [ProductCategoryController::class, 'destroy'])->name('destroy');
 });
