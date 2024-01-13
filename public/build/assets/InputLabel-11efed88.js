896a9aea15c1ceef559ebfc43cf34423491dd5cc
Tesimune <tesimune@gmail.com>
2024-01-13 06:22:18 +0100

    Build updated

--- public/build/assets/InputLabel-11efed88.js
+++
@@ -1,1 +0,0 @@
-import{j as m}from"./app-7ca0f220.js";function o({value:t,className:e="",children:r,...s}){return m.jsx("label",{...s,className:"block font-medium text-sm text-gray-700 "+e,children:t||r})}export{o as I};
