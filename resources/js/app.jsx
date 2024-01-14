50fdac15fdaad0e082c5245b82f6d1d27ee3a40b
Tesimune <tesimune@gmail.com>
2024-01-14 09:28:32 +0100

    Updated loader

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -9,13 +9,18 @@ const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.jsx`,
+            import.meta.glob("./Pages/**/*.jsx")
+        ),
     setup({ el, App, props }) {
         const root = createRoot(el);
 
         root.render(<App {...props} />);
     },
     progress: {
-        color: '#4B5563',
+        color: "#daa520",
+        showSpinner: "true",
     },
 });
